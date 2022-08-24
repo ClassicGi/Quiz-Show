3a1c6d4f119116f6dde8124ac08dee1cb4c2715b
ClassicGi <107224938+ClassicGi@users.noreply.github.com>
2022-08-24 01:44:30 +0000

    Updated quiz-show project

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ print("Q1. Electrons move faster than the speed of light.")
 print()
 
 # Prompt user to enter T or F
-questionOne = input("Please enter 'T' for true or 'F' for false:  ")
-print()
+answer= input("Please enter 'T' for true or 'F' for false:  ")
 
-# Create a loop if question it is true or not
-while questionOne not in "False":
-  questionOne = input("Please enter 'T' for true or 'F' for false:  ")
-else:
-  pass
+# Loop through each input have entered valid answer
+while answer != "T" and answer != "F":
+  answer = input("Please enter 'T' for true or 'F' for false:  ")
+
+print("Your Q1 answer: ", answer)
+print("----------------------------------------------------")
 print()
 
 # Display question 2
@@ -34,14 +34,14 @@ print("Q2. Light travels in a straight line.")
 print()
 
 # Prompt user to enter T or F
-questionTwo = input("Please enter 'T' for true or 'F' for false: ")
-print()
+answer = input("Please enter 'T' for true or 'F' for false:  ")
 
-# Create a loop if question it is true or not
-while questionTwo not in "True":
-  questionTwo = input("Please enter 'T' for true or 'F' for false:  ")
-else:
-  pass
+# Loop through each input have entered valid answer
+while answer != "T" and answer != "F":
+  answer = input("Please enter 'T' for true or 'F' for false:  ")
+
+print("Your Q2 answer: ", answer)
+print("----------------------------------------------------")
 print()
 
 # Display question 3
@@ -49,14 +49,14 @@ print("Q3. The Atlantic Ocean is the biggest ocean on Earth.")
 print()
 
 # Prompt user to enter T or F
-questionThree = input("Please enter 'T' for true or 'F' for false: ")
-print()
+answer = input("Please enter 'T' for true or 'F' for false:  ")
 
-# Create a loop if question it is true or not
-while questionThree not in "False":
-  questionThree = input("Please enter 'T' for true or 'F' for false:  ")
-else:
-  pass
+# Loop through each input have entered valid answer
+while answer != "T" and answer != "F":
+  answer = input("Please enter 'T' for true or 'F' for false:  ")
+
+print("Your Q3 answer: ", answer)
+print("----------------------------------------------------")
 print()
 
 # Display question 4
@@ -64,22 +64,21 @@ print("The total length of the Great Wall of China adds up to 13,171 miles.")
 print()
 
 # Prompt user to enter T or F
-questionFour = input("Please enter 'T' for true or 'F' for false: ")
-print()
+answer = input("Please enter 'T' for true or 'F' for false:  ")
 
-# Create a loop if question it is true or not
-while questionFour in "False":
-  questionFour = input("Please enter 'T' for true or 'F' for false:  ")
-else:
-  pass
+# Loop through each input have entered valid answer
+while answer != "T" and answer != "F":
+  answer = input("Please enter 'T' for true or 'F' for false:  ")
+
+print("Your Q4 answer: ", answer)
+print("----------------------------------------------------")
 print()
 
 # Create the tuples to hold the questions and answers
 questions = ('Q1. Electrons move faster than the speed of light.', 'Q2. Light travels in a straight line.', 'Q3. The Atlantic Ocean is the biggest ocean on Earth.', 'Q4. The total length of the Great Wall of China adds up to 13,171 miles.')
 answers = ('F', 'T', 'F', 'T')
 
-
-
+# Go through each question and answer pair
 for index in range(len(questions)):
   print(f"Question {index+1}")
   print(f"index: {index}")
@@ -91,7 +90,7 @@ for index in range(len(questions)):
 totalQuestions = 4
 userAnswer = 0
 for answer in answers:
-  if userAnswer != answers[1]:
+  if userAnswer == answers[1]:
     userAnswer += 1
 
 # Display total number of correct answers
